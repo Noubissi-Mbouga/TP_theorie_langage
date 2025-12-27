1abbb643788789fb371cb3434ad7a065e5e41f40
Noubissi-Mbouga <igorwilfriednoubissi@gmail.com>
2025-12-27 05:19:06 +0000

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -14,6 +14,50 @@ def setup_graphviz():
 
 setup_graphviz()
 
+class Automate:
+    def __init__(self):
+        self.states = set()
+        self.start_state = None
+        self.final_states = set()
+        self.transitions = {}
+
+
+def grammaire_vers_automate(regle, axiome):
+    auto = Automate()
+    auto.start_state = axiome
+
+    # Ajouter les états
+    for A in regle:
+        auto.states.add(A)
+
+    # Ajouter un état final unique F
+    auto.states.add("F")
+    auto.final_states.add("F")
+
+    for A, productions in regle.items():
+        for prod in productions:
+
+            if prod in ("epsilon", "ε"):
+                # A est un état final
+                auto.final_states.add(A)
+                continue
+            
+            if len(prod) == 1:
+                # A -> a
+                sym = prod
+                auto.transitions.setdefault(A, {})
+                auto.transitions[A][sym] = "F"
+
+            elif len(prod) == 2:
+                # A -> aB
+                sym = prod[0]
+                B = prod[1]
+                auto.transitions.setdefault(A, {})
+                auto.transitions[A][sym] = B
+
+    return auto
+
+
 def draw_nfa(nfa, filename="nfa"):
     dot = Digraph(comment="NFA", format="png")
     dot.attr(rankdir='LR')
@@ -46,6 +90,7 @@ def draw_dfa(dfa, filename="dfa"):
             dot.edge(s, tgt, label=sym)
     path = dot.render(filename, cleanup=True)
     return path
+
 
 
 
